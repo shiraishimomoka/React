a98b2e71af447831109bdc1beddeee88f1a33f32
shiraishimomoka <momomomo.momo1127@gmail.com>
2021-01-16 18:19:42 +0900

    コンポーネントのpropsとstate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import { List } from "./List";
 
 function App() {
+  const [description, setDescription] = useState('クリック前の表示');
+
+  const changeDescription = () => {
+    setDescription('クリック後の表示です。')
+  }
+
   return (
-    <div>ゼロから始めるReact入門</div>
+    <div>
+      { description }
+      <List title="取り扱い言語一覧" />
+      <button onClick={changeDescription}>ボタン</button>
+    </div>
   );
 }
 
