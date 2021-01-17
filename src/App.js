70568c1c7209a42a6bdce7df96a3094a85ea0b22
shiraishimomoka <momomomo.momo1127@gmail.com>
2021-01-17 09:47:49 +0900

    条件分岐と繰り返し

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { useState } from "react";
 import { List } from "./List";
+import { Form } from "./Form"
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { description: 'クリック前の表示' }
-  }
-  changeDescription() {
-    this.setState({
-      description: 'クリック後の表示です。'
-    })
-  }
-  render() {
-    const { description } = this.state;
-    return (
-      <div>
-        { description }
-        <List title="取り扱い言語一覧" /> 
-        <button onClick={() => this.changeDescription()}>
-          ボタン
-        </button> 
-      </div>
-    )
-  }
+function App() {
+  const [tab, setTab] = useState('list');
+
+  return (
+    <div>
+      <header>
+        <ul>
+          <li onClick = {() => setTab('list')}>リスト</li>
+          <li onClick = {() => setTab('form')}>フォーム</li>
+        </ul>
+      </header>
+      <hr/>
+
+    {
+      tab === 'list' ? <List /> : <Form />
+    }
+    </div>
+  );
 }
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,13 +1,20 @@
-import React from 'react';
+const LANGUAGES = [
+    'JavaScript',
+    'C++',
+    'Ruby',
+    'Java',
+    'PHP',
+    'Go'
+];
 
-export class List extends React.Component {
-    render() {
-        const { title } = this.props;
-        return (
-            <div>
-                <h4>{ title }</h4>
-                <div>リストです</div>
-            </div>
-        )
-    }
+export const List = () => {
+    return (
+        <div>
+            {
+                LANGUAGES.map((lang, index) => {
+                    return <div key={index}>{ lang }</div>
+                })
+            }
+        </div>
+    )
 }
